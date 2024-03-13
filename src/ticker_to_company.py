7dcd44f56016e401133d02995c8f979be9678925
Roni Hogri <144431961+ronihogri@users.noreply.github.com>
2024-03-13 11:40:11 +0100

    Add files via upload
    Module containing a custom class for retrieving different kinds of information regarding a company based on the ticker symbol of its stock. 
    Can be used, for example, to get features relevant for model training without having to explicitly retrieve this info from the SQL database.

---
+++ src/ticker_to_company.py
@@ -0,0 +1,275 @@
+'''
+Module containing the custom class "Company" for providing details regarding the company issuing a specific stock on-the-fly, 
+without having to check the SQL database.
+
+***Note: that this class refers to a specific table ('Stocks') within SQLite file. 
+This is so that you don't have to specify it each time.
+However, you can specify a different path and/or table name and pass them to the class if you'd like 
+(just make sure that the table you specify is arranged in a similar way to 'Stocks'.*** 
+
+Example:
+    from ticker_to_company import Company
+
+    # Create a Company instance:
+    company = Company("GOOGL")
+    company_differet_DB = Company("GOOGL", './different_sqlite_file.sqlite', 'different_table_name')
+
+    # Retrieve company name and main market in which the stock is traded:
+    name, market = company.name(), company.market()
+    
+    # Retrieve city and country of company's HQ:
+    city, country = company.hq().city(), company.hq().country()
+    
+    # Check if company HQ is located in the USA:
+    in_usa = company.inusa()        
+  
+'''
+
+sqlite_file_path = './S&P 500.sqlite' #default path to the sqlite database
+table_name = 'Stocks'
+
+#required libraries
+import sqlite3
+import requests
+from bs4 import BeautifulSoup
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="hq_locator") #geolocator object for getting geographic data about the company
+
+
+
+class Company :
+    """A class representing a company with methods to retrieve various attributes and information."""
+    
+    def __init__(self, symbol, sql_path=sqlite_file_path, table=table_name) :
+        """Initializes a Company object with the provided symbol and connects to the SQLite database.
+
+        Args:
+            symbol (str): The company's ticker.
+            sql_path (str, optional): The path to the SQLite database file. Defaults to './S&P 500.sqlite'.
+
+        Returns:
+            None
+            
+        Raises:
+            TypeError: If one of the input variables is not a str.
+            ValueError: If the inputted symbol does not exist in the specified SQLite table.
+        """
+        
+        for input_var in (symbol, sql_path, table): #make sure inputs are of the correct type (str)
+            if not isinstance(input_var, str):
+                raise TypeError(f"Input must be of type 'str', but input of type {type(input_var)} encountered.")
+            
+        self.symbol = symbol.upper()
+        self.conn = sqlite3.connect(sql_path)
+        self.cur = self.conn.cursor()
+        self.cur.execute(f"SELECT Symbol FROM {table_name} WHERE Symbol = '{self.symbol}'")
+        symbol_exists = self.cur.fetchone()
+        if symbol_exists is None :
+            raise ValueError(f"Symbol '{self.symbol}' does not exist in the '{table_name}' table in {sql_path}.")  
+                   
+        
+       
+    def fetch_attribute(self, attribute_name, query):
+        """Fetches an attribute of the company from the SQLite database using the provided SQL query.
+
+        Args:
+            attribute_name (str): The name of the attribute to fetch.
+            query (str): The SQL query to execute.
+
+        Returns:
+            str: The fetched attribute value.
+        """
+        
+        self.cur.execute(query)
+        result = self.cur.fetchone()
+        if result is not None:
+            setattr(self, attribute_name, result[0])
+        else:
+            setattr(self, attribute_name, 'NA')
+        return getattr(self, attribute_name)
+     
+
+    def name(self):
+        """Returns the name of the company issuing the stock.
+
+        Returns:
+            str: Company name.
+        """
+        
+        query = f"SELECT Security FROM {table_name} WHERE Symbol = '{self.symbol}'"
+        self._name = self.fetch_attribute('_name', query).split('(')[0].strip()
+        return self._name
+        
+        
+    def sector(self):
+        """Returns the business sector in which this company operates.
+
+        Returns:
+            str: Company sector.
+        """
+        
+        query = f"SELECT GICS_Sector FROM {table_name} WHERE Symbol = '{self.symbol}'"
+        return self.fetch_attribute('_sector', query)
+        
+
+    def subsector(self):
+        """Returns the sub-sector in which this company operates.
+
+        Returns:
+            str: Company sub-sector.
+        """
+        
+        query = f"SELECT GICS_Sub_Industry FROM {table_name} WHERE Symbol = '{self.symbol}'"
+        return self.fetch_attribute('_subsector', query)
+        
+
+    def CIK(self) :
+        """Returns the company's CIK (Central Index Key), as issued by the SEC.
+        
+        Returns:
+            str: The company's CIK.
+        """
+        query = f"SELECT CIK FROM {table_name} WHERE Symbol = '{self.symbol}'"
+        return self.fetch_attribute('_CIK', query)    
+
+
+    def wiki_soup(self) : 
+        """Retrieves information about the company from Wikipedia.
+
+        This method constructs a Wikipedia URL based on the company's name,
+        fetches the content of the webpage, and parses it using BeautifulSoup
+        to extract relevant information.
+
+        Returns:
+            BeautifulSoup object: Parsed HTML content of the Wikipedia page,
+            or 'NA' if the page cannot be accessed.
+        """
+        
+        name = self._name.replace(" ", "_")
+        url = 'https://en.wikipedia.org/wiki/' + name
+        r = requests.get(url)
+        if r.status_code != 200 : return 'NA'
+        content = requests.get(url).content
+        self._soup = BeautifulSoup(content, 'html.parser')
+        return self._soup
+        
+
+    def market(self) : 
+        """Returns the main market in which this stock is traded.
+
+        This method extracts the main market information from the Wikipedia page
+        obtained by calling the wiki_soup() method. It searches for the 'Traded as'
+        label in the infobox vcard table on the page and returns the corresponding
+        market name.
+
+        Returns:
+            str: The main market in which this stock is traded, 
+            or 'NA' if the information is not available or cannot be retrieved.
+        """
+        soup = self.wiki_soup()
+        if (soup == 'NA') or (soup is None) : return 'NA'
+        results = soup('table', attrs={"class":"infobox vcard"})
+        for result in results :
+            hrefs = result(href=True)
+            for i, href in enumerate(hrefs) :
+                if 'Traded as' in hrefs[i-1] :
+                    self._market = href.text
+                    return self._market   
+                    
+        
+
+    def hq(self):
+        """ Retrieves the headquarters location of the company from the database.
+
+        This method should be called before accessing other methods that depend on
+        the headquarters location attribute.
+
+        Returns:
+            Company: The Company object with updated headquarters location attribute.
+
+        Note:
+            This method does not return the headquarters location directly! 
+            It is intended to be called as a prerequisite for other methods that depend on the HQ location.
+        """
+        
+        query = f"SELECT Headquarters_Location FROM {table_name} WHERE Symbol = '{self.symbol}'"
+        self._hq_location = self.fetch_attribute('_hq_location', query)
+        return self
+
+
+    """Subdefs of hq():"""
+    
+    def location(self):
+        """ Retrieves the company's HQ location as specified in the SQLite database.
+        
+        Returns:
+            str: HQ location.
+        """
+        return self._hq_location
+    
+    
+    def city(self): 
+        """ Retrieves the city in which the company's HQ is located.
+        
+        Returns:
+            str: HQ city.
+        """
+        return self._hq_location.split(',')[0].strip()
+        
+        
+    def inusa(self):
+        """ Determines if the company's headquarters is located in the United States.
+        
+        The 'Stocks' table contains the HQ location as "city, state" for US-based companies (and "city, country" for companies with HQ outside of the USA). 
+        Therefore, the geolocator module is used to check if a given location refers to the USA. 
+
+        Returns:
+            bool: True if the headquarters is in the United States, False otherwise.
+        """
+        
+        location = geolocator.geocode(self._hq_location)
+        country = location.raw['display_name'].split(',')[-1].strip()
+        return country == 'United States'
+
+    
+    def state(self):
+        """ For US- based companies, returns the state where the company's headquarters is located.
+
+        Returns:
+            str: The state name if the headquarters is in the United States, otherwise 'NA'.
+        *I am aware that many other countries have states too, this is just how the wikipedia table (and therefore the 'Stocks' table) is organized... 
+        """
+        
+        if self.inusa() :
+            return self._hq_location.split(',')[-1].strip()
+        else :
+            return 'NA'      
+            
+
+    def country(self):
+        """ Retrieves the country in which the company's HQ is located.
+        
+        Returns:
+            str: HQ country.
+        """
+        
+        if self.inusa() :
+            return 'United States'
+        else :
+            return self._hq_location.split(',')[-1].strip()   
+
+
+    def coords(self):
+        """ Retrieves the geographic coordinates (latitude, longitude) of the company's HQ
+
+        Returns:
+            A tuple containing 2 float elements: (latitude, longitude), as retrieved by the geolocator.
+        
+        """
+        
+        location = geolocator.geocode(self._hq_location)
+        self._coords = (location.latitude, location.longitude) 
+        return self._coords 
+
+    
